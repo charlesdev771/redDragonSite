eeaf13f4eb46c009e6679fe2c1f7743bf151114e
charlesdev771 <charlesdev0721@protonmail.com>
2022-06-21 05:33:26 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react'
+import DragonIcon from './imgs/icon.png';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div id="body_background">
+      <img src={DragonIcon}/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
