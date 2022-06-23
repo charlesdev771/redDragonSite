ace7c9acfa176c8a282e0c8b394c274b160ba496
charlesdev771 <charlesdev0721@protonmail.com>
2022-06-23 04:02:07 +0000

    First version

--- src/App.js
+++ src/App.js
@@ -7,9 +7,28 @@ import DragonIcon from './imgs/icon.png';
 function App() {
   return (
     <div className="App">
+
       <div id="body_background">
-      <img src={DragonIcon}/>
+
+        <div id="title">
+          <h1 id="title_main">Red Dragon</h1>
+        </div>
+
+        <div id="img_dragon">
+          <img src={DragonIcon}/>
+        </div>
+
+        <div id="buttons">
+          <button type="button" class="btn btn-danger">Download</button>
+          <button type="button" class="btn btn-dark">Documentation</button>
+        </div>
+
+        <div id="footer">
+          <h3>Created by: Charles Dantas (chameleon)</h3>
+        </div>
+
       </div>
+
     </div>
   );
 }
